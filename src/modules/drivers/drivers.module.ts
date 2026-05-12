0f591f77ec9483d26bb1726eda625501fd824ef1
khaled-alkara <kaledalkara@gmail.com>
2026-05-12 09:39:41 -0700

    create drivers.module.ts

--- src/modules/drivers/drivers.module.ts
+++ src/modules/drivers/drivers.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { DriversController } from './drivers.controller';
 import { DriversService } from './drivers.service';
+import { Driver } from './entities/driver.entity';
+import { DriverLocation } from './entities/driver-location.entity';
+import { User } from '../users/entities/user.entity';
+import { Order } from '../orders/entities/order.entity';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Driver, DriverLocation, User, Order]),
+    NotificationsModule,
+  ],
   controllers: [DriversController],
-  providers: [DriversService]
+  providers: [DriversService],
+  exports: [DriversService],
 })
-export class DriversModule {}
+export class DriversModule { }
